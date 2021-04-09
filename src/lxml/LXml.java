01429afcf8f935b722a1e51221f4c485544a8d08
Usuario <Usuario@DESKTOP-KQGVQ20>
2021-04-09 13:57:33 -0500

    xml

--- src/lxml/LXml.java
+++ src/lxml/LXml.java
@@ -22,30 +22,6 @@ public class LXml {
         Docente [] docentes=null;
         docentes = c.leerXML();
         c.insertarXml(docentes);
-//        try{
-//            File archivo = new File("datos.xml");
-//            DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
-//            DocumentBuilder db = dbf.newDocumentBuilder();
-//            Document d=db.parse(archivo);
-//            d.getDocumentElement().normalize();
-//            System.out.println("elemento principal:"+d.getDocumentElement().getNodeName());
-//            cargando todos los docentes en una coleccion de tipo nodo
-//            NodeList listDocentes = d.getElementsByTagName("docente");
-//            
-//            for(int i=0;i<listDocentes.getLength();i++){
-//                Node nodo=listDocentes.item(i);
-//                System.out.println("Docente:"+nodo.getNodeName()+i);
-//                if(nodo.getNodeType()==Node.ELEMENT_NODE){
-//                    Element element =(Element)nodo;
-//                    System.out.println("id:"+element.getAttribute("id"));
-//                    System.out.println("Nombre:"+element.getElementsByTagName("nombre").item(0).getTextContent());
-//                    System.out.println("username:"+element.getElementsByTagName("username").item(0).getTextContent());
-//                    System.out.println("password: " + element.getElementsByTagName("321423").item(0).getTextContent());
-//                }
-//            }
-//        }catch (Exception e) {
-//            e.printStackTrace();
-//    }
     
     }
 }
